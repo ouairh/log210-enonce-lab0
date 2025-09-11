fbba11477f841966e843ddb85c04461aabeb11bd
ouairh <rayan.hani.1@ens.etsmtl.ca>
2025-09-11 11:20:36 -0400

    t

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,24 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
+import { request } from 'http';
 import 'jest-extended';
 
 describe('redemarrerJeu.test.ts', () => {
   it("devrait implémenter test", async () => {
     throw new Error("Ce test n'a pas été défini")
   });
+});
+
+
+describe('GET /api/v1/jeu/redemarrerJeu', ()=> {
+  beforeAll(async ()=>{
+    await request()
+    
+    
+
+
+  })
+
+
+
 });
